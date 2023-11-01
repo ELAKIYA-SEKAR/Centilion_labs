331b4e76e030bbc1fe4d357000f30de65d7f6542
ELAKIYA-SEKAR <meelakiya24@gmail.com>
2023-11-02 01:19:10 +0530

    update

--- hugging_Face.py
+++ hugging_Face.py
@@ -1,12 +1,30 @@
+from flask import Flask, render_template, request, jsonify
 from diffusers import DiffusionPipeline
 import torch
 
+app = Flask(__name)
+
+# Initialize the DiffusionPipeline and set it to run on GPU
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe.to("cuda")
 
-# if using torch < 2.0
-# pipe.enable_xformers_memory_efficient_attention()
+# Define the home route to render the HTML UI
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-prompt = "An astronaut riding a green horse"
+# Define the route to handle image generation
+@app.route('/generate_image', methods=['POST'])
+def generate_image():
+    text = request.form.get('text')
+    
+    # Generate an image using the text-to-image model
+    images = pipe(prompt=text).images[0]
+    
+    # Convert the image tensor to bytes
+    image_bytes = images.numpy().tobytes()
+    
+    return image_bytes
 
-images = pipe(prompt=prompt).images[0]
+if __name__ == '__main__':
+    app.run(debug=True)
